5e774a328f659b794255a88b36c7f34762b1526f
abdelfatahfouad97 <abdelfatahfouad1997@gmail.com>
2016-07-20 13:47:29 +0200

    Create tajriba.js

---
+++ tajriba.js
@@ -0,0 +1,20 @@
+<b:include data='blog' name='all-head-content'/>
+<meta content='android4technology' name='title'/>
+<meta content='دروس تكنولوجيا وتقنية وشرح برامج والعاب' name='description'/>
+<meta content='برامج والعاب كمبيوتر,تهكير العاب,دروس تكنولوجيا وتقنية,شرح برامج والعاب ,اندرويد' name='keywords'/>
+<meta content='تكنولوجياالاندرويد' name='Author'/>
+<meta content='abdelfatahfouad1997@gmail.com' name='Email'/>
+<meta content='global' name='distribution'/>
+<meta content='5 days' name='revisit'/>
+<meta content='5 days' name='revisit-after'/>
+<meta content='document' name='resource-type'/>
+<meta content='all' name='audience'/>
+<meta content='general' name='rating'/>
+<meta content='all' name='robots'/>
+<meta content='index, follow' name='robots'/>
+<meta content='ar' name='language'/>
+<meta content='Egypt' name='country'/>
+<meta content='blogger' name='generator'/>
+<meta content='(c) 2016' name='copyright'/>
+
+
